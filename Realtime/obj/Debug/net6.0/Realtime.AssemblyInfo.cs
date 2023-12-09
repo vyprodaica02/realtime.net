2bb024aedbb2821ec0f07a6f446adbcb1cc48aa0
vyprodaica02 <duc.luongming@gmail.com>
2023-12-09 16:55:04 +0700

    oke

--- Realtime/obj/Debug/net6.0/Realtime.AssemblyInfo.cs
+++ Realtime/obj/Debug/net6.0/Realtime.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Realtime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f14286de239ccdadbc469f79991c8779ee50fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea3bfca0efcdff2c210d56bcb213457be17eb0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Realtime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Realtime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
